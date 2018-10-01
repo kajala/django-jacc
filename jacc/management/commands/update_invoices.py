507050c4847295e62ab747592f2280a53d57f3aa
Jani Kajala <kajala@gmail.com>
2018-10-01 13:09:49 -0500

    update invoices tweaks

--- jacc/management/commands/update_invoices.py
+++ jacc/management/commands/update_invoices.py
@@ -7,11 +7,12 @@ from jutil.command import SafeCommand
 
 
 class Command(SafeCommand):
-    help = 'Updates cached values of invoices with due dates +- 60d of current date'
+    help = 'Updates cached values of invoices'
 
     def add_arguments(self, parser: CommandParser):
         parser.add_argument('--invoice', type=int)
         parser.add_argument('--force', action='store_true')
+        parser.add_argument('--verbose', action='store_true')
 
     def do(self, *args, **options):
         invoices = Invoice.objects.all()
@@ -20,7 +21,12 @@ class Command(SafeCommand):
         if not options['force']:
             invoices = invoices.filter(close_date=None)
 
+        count = 0
         for invoice in invoices.order_by('id'):
             assert isinstance(invoice, Invoice)
-            print('updating', invoice)
+            if options['verbose']:
+                print('Updating', invoice)
             invoice.update_cached_fields()
+            count += 1
+
+        print('Updated', count, 'invoices')
--- project/settings.py
+++ project/settings.py
@@ -73,8 +73,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'django_jacc',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
+        'USER': 'dev',
+        'PASSWORD': 'dev',
         'HOST': 'localhost',
         'PORT': 5432,
         'CONN_MAX_AGE': 180,
