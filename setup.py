7c1caa03d4b0f2cd2318c83fbffe9b3cea9ba2b8
Jani Kajala <kajala@gmail.com>
2019-02-19 14:33:11 +0200

    Release 2.1.15

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ install_requires = parse_requirements('requirements.txt', session=False)
 
 setup(
     name='django-jacc',
-    version='2.1.14',
+    version='2.1.15',
     author=u'Jani Kajala',
     author_email='kajala@gmail.com',
     packages=find_packages(exclude=['project', 'venv']),
