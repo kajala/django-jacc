adfc1e93f6f6e2915c22068b39b880a0f4686943
Jani Kajala <kajala@gmail.com>
2020-05-26 09:07:39 -0500

    py.typed and mypy support

--- setup.py
+++ setup.py
@@ -22,6 +22,6 @@ setup(
     license='MIT licence, see LICENCE.txt',
     description='Simple double entry accounting system (debits/credits) for Django projects.',
     long_description=open('README.md').read(),
-    zip_safe=True,
+    zip_safe=False,
     install_requires=install_requires
 )
