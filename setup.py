c5da9f93608189dc6ca8608e9f7bc409622bf169
Jani Kajala <kajala@gmail.com>
2020-06-23 12:02:37 -0500

    Release 3.4.6

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ install_requires = parse_requirements('requirements.txt', session=False)
 
 setup(
     name='django-jacc',
-    version='3.4.5',
+    version='3.4.6',
     author=u'Jani Kajala',
     author_email='kajala@gmail.com',
     packages=find_packages(exclude=['project', 'venv']),
