f7dfa6ab330e9fc3201188ebb4b93e0e7142d628
Jani Kajala <kajala@gmail.com>
2020-06-28 22:47:15 -0500

    Release 3.4.8

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ install_requires = parse_requirements('requirements.txt', session=False)
 
 setup(
     name='django-jacc',
-    version='3.4.7',
+    version='3.4.8',
     author=u'Jani Kajala',
     author_email='kajala@gmail.com',
     packages=find_packages(exclude=['project', 'venv']),
