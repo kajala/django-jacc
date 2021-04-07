1c76b0217dbd22c7a4f816466ca3656bbbd2f57a
Jani Kajala <kajala@gmail.com>
2021-04-07 11:32:49 -0500

    django 3.2 compatibility change

--- jacc/apps.py
+++ jacc/apps.py
@@ -5,3 +5,4 @@ from django.utils.translation import gettext_lazy as _
 class JaccConfig(AppConfig):
     name = "jacc"
     verbose_name = _("Accounting")
+    default_auto_field = "django.db.models.AutoField"
