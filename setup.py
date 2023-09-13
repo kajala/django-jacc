6e7b926ecf64bdf58e486f8ab2e7ea6899ff23a7
Jani Kajala <kajala@gmail.com>
2023-09-12 20:05:09 -0500

    Release 3.11.12

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ install_requires = parse_requirements("requirements.txt", session=False)
 
 setup(
     name="django-jacc",
-    version="3.11.11",
+    version="3.11.12",
     author="Jani Kajala",
     author_email="kajala@gmail.com",
     packages=find_packages(exclude=["project", "venv"]),
